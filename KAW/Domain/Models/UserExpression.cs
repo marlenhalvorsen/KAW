b1fbcb6dc373a3e07929ae2e97fd58ceb262e40d
marlenhalvorsen <marlenhalvorsen@gmail.com>
2025-10-15 14:59:51 +0200

    Added interface for AppDbContext, added pk for UserExpressions, made req to ensure name  for expression and eaach expressionName is unique.

--- KAW/Domain/Models/UserExpression.cs
+++ KAW/Domain/Models/UserExpression.cs
@@ -2,6 +2,7 @@
 {
     public class UserExpression
     {
+        public int Id { get; set; }
         public string? Name { get; set; }
         public string? Description { get; set; }
 
---
+++ KAW/Infrastructure/Persistence/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using KAW.Application.Interfaces;
+using KAW.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks; 
+
+namespace KAW.Infrastructure.Persistence
+{
+    public class AppDbContext : DbContext, IAppDbContext
+    {
+        public DbSet<UserExpression> UserExpressions { get; set; } = null!; 
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<UserExpression>(entity =>
+            {
+                entity.HasKey(e => e.Id);       //set id as pk
+                entity.Property(e => e.Name)
+                .HasMaxLength(200)
+                .IsRequired();                  //name is required
+                entity.HasIndex(e => e.Name).IsUnique(); //name is also supposed to be unique
+            });
+        }
+        public Task<int> SaveChangesAsync(CancellationToken ct = default)
+                => base.SaveChangesAsync(ct);
+    }
+}
